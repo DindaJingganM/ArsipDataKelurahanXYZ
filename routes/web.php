a4834d1737e422a340ec8eaea835f3abd8ed3497
DindaJingganM <dindajinggan20@gmail.com>
2021-10-23 10:49:31 +0700

    merubah route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/arsip', 'ArsipController@index');
+Route::get('/user', 'UserController@index');
